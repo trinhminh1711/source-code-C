43be7d7c2798ce34f4569f62c102728df6254664
= <=>
2019-04-08 21:12:53 +0700

    mihdeptrai

--- week2/bai4/main.c
+++ week2/bai4/main.c
@@ -3,36 +3,31 @@
 #include<math.h>
 int main()
 {
-    int i,n,a;
+    int i=1,n,a;
     printf("input :");
     scanf("%d",&n);
+    if( n>=5||n<2)
     {
-        if( n>=5||n<2)
+        printf("n phai lon hon 1 hoac nho hon 5\n");
+        printf("nhap lai n :");
+        scanf("%d",&a);
+        while(11*i<pow(10,a))
         {
-            printf("n phai lon hon 1 hoac nho hon 5\n");
-            printf("nhap lai n :");
-            scanf("%d",&a);
-             for(i=1; i<pow(10,a); i++)
-        {
-            if(i%(11)==0)
-            {
-                printf("%d\n",i);
-            }
+
+            printf("%d\n ",11*i);
+            i++;
         }
 
-        }
-        else if(n>=2&&n<5)
+    }
+    else if(n>=2&&n<5)
+    {
+
+        while(11*i<pow(10,n))
         {
-        {
-        for(i=1; i<pow(10,n); i++)
-        {
-            if(i%(11)==0)
-            {
-                printf("%d\n",i);
-            }
+            printf("%d\n",11*i);
+            i++;
         }
-        }
-        }
+    }
+
     getch();
-}
 }
